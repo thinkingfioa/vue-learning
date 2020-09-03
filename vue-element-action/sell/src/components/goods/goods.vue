7e6e315bae286a99c965d541e8f1e4fb48551056
thinking <thinking_fioa@163.com>
2020-09-03 23:19:36 +0800

    goods content

--- vue-element-action/sell/src/components/goods/goods.vue
+++ vue-element-action/sell/src/components/goods/goods.vue
@@ -3,14 +3,39 @@
     <div class="menu-wrapper">
       <ul>
         <li v-for="item in goods" :key="item.index" class="menu-item">
-          <span class="text">
+          <span class="text border-1px">
             <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>
             {{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper"></div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="item in goods" :key="item.index" class="foods-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" :key="food.index" class="food-item">
+              <div class="icon">
+                <img :src="food.icon" width="57px" height="57px">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="sellCount">月售{{food.sellCount}}份</span>
+                  <span class="rating">好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="newPrice"><span class="unit">¥</span>{{food.price}}</span>
+                  <span v-show="food.oldPrice" class="oldPrice">¥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -85,8 +110,60 @@ export default {
           display table-cell
           width 56px
           vertical-align middle
-          border-bottom 1px solid rgba(7, 17, 27, 0.1)
+          border-1px(rgba(7, 17, 27, 0.1))
           font-size 12px
     .foods-wrapper
       flex 1
+      .foods-list
+        .title
+          padding-left 14px
+          line-height 26px
+          height 26px
+          background #f3f5f7
+          border-left 2px solid #d9dde1
+          font-size 12px
+          color rgb(147, 153, 159)
+        .food-item
+          display flex
+          margin 18px
+          padding-bottom 18px
+          border-1px(rgba(7, 17, 27, 0.1))
+          &:last-child
+            border-none()
+            margin-bottom 0px
+          .icon
+            flex 0 0 57px
+            margin-left 10px
+          .content
+            flex 1
+            margin-left 18px
+            font-size 0
+            .name
+              margin-top 2px
+              font-size 14px
+              color rgb(7, 17, 27)
+              line-height 14px
+              height 14px
+            .desc
+              margin-top 8px
+              font-size 10px
+              color rgb(147, 153, 159)
+              line-height 10px
+            .extra
+              margin-top 8px
+              font-size 10px
+              color rgb(147, 153, 159)
+              line-height 10px
+              .sellCount
+                margin-right 12px
+            .price
+              margin-top 8px
+              .newPrice
+                font-size 14px
+                color rgb(147, 153, 159)
+                font-weight 700
+                line-height 24px
+                .unit
+                  font-size 10px
+                  font-weight normal
 </style>
